c9ff713ca4527ec35d73bda30cda9bb4de377f4e
arkannaufal1811 <arkn3418@gmail.com>
2026-08-05 14:46:32 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,12 +2,10 @@
 $host = getenv('MYSQLHOST') ?: 'mysql.railway.internal';
 $user = getenv('MYSQLUSER') ?: 'root';
 $pass = getenv('MYSQLPASSWORD') ?: 'frYLhKUxtPywqHDZPMVZzvLEtdaFVCOY';
-$db   = getenv('MYSQLDATABASE') ?: 'railway';
+
+// UBAH BARIS INI:
+$db   = 'db_bekakak_ayam'; // Ubah jadi db_bekakak_ayam
+
 $port = getenv('MYSQLPORT') ?: 3306;
 
 $koneksi = mysqli_connect($host, $user, $pass, $db, $port);
-
-if (!$koneksi) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
-?>
