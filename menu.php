214e1dabbf87cd2001e7c6d8e7ec035c767a09e8
arkannaufal1811 <arkn3418@gmail.com>
2026-08-05 15:05:24 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
 $page_title = 'Menu & Harga — Bekakak Ayam Mang Atang';
 $active = 'menu';
 require 'config/db.php';
